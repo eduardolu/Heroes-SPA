275819fe79b4f3073dc4aca00ee366d000aaa5d2
Edu lu <edulu@MacBook-Pro-de-Edu.local>
2026-05-09 12:04:39 +0200

    fix: redirect to login if user is not authenticated

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Navbar } from '../../UI'
 import { DcPage, HeroePage, MarvelPage, SearchPage } from '../pages'
+import { useUser } from '../../context/UserContext'
 
 export const HeroesRoutes = () => {
+  const { user } = useUser()
+
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
+
   return (
     <>
         <Navbar />
